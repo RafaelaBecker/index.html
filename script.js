2a209a3df629cb6381e7b64447cda61bd851071d
RafaelaBecker <5319214@alunouninter.com>
2025-05-15 17:39:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,3 @@ document.getElementById("mostrarMensagem").onclick = function () {
     container.innerHTML = `<p>Seja muito bem-vindo!</p>`;
   }
 };
-
-    document.body.appendChild(container);
-
-    // Quando clicar na imagem, aparece "Miau"
-    const imagem = container.querySelector("img");
-    imagem.addEventListener("click", () => {
-      alert("Miau");
-    });
-  }
-};
