26e997040389cd5be7c2dde5ea2aea186f922293
RafaelaBecker <5319214@alunouninter.com>
2025-05-15 15:58:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ botao.onclick = function () {
         mensagemCriada = true;
     }
 };
+
+document.getElementById("mostrarMensagem").onclick = function () {
+  let container = document.getElementById("mensagemContainer");
+
+  if (!container) {
+    container = document.createElement("div");
+    container.id = "mensagemContainer";
+    container.innerHTML = `
+      <p>Seja muito bem-vindo!</p>
+      <img src="https://www.w3schools.com/w3images/fjords.jpg" alt="Imagem da galeria">
+    `;
+    document.body.appendChild(container);
+
+    const imagem = container.querySelector("img");
+    imagem.addEventListener("click", () => {
+      alert("Miau");
+    });
+  }
+};
