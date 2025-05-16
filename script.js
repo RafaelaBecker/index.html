aaff782e319bee7ef391f64a7e9838020eb95289
RafaelaBecker <5319214@alunouninter.com>
2025-05-16 17:47:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ document.getElementById("mostrarMensagem").onclick = function () {
   const container = document.getElementById("mensagemContainer");
 
   if (container.classList.contains("show")) {
-    // Se já está mostrando, esconde a mensagem
     container.classList.remove("show");
   } else {
-    // Se não está mostrando, mostra a mensagem e adiciona o texto se estiver vazio
     if (container.innerHTML.trim() === "") {
       container.innerHTML = `<p>Seja muito bem-vindo! 🐱✨</p>`;
     }
